f0806544745493e14570993e159bd94d36233853
bandenawaz <bandenawaz.bagwan@gmail.com>
2026-02-23 14:03:15 +0530

    Adding files Spring Framework IOC

--- day16/securebank/src/main/java/com/securebank/App.java
+++ day16/securebank/src/main/java/com/securebank/App.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.example.config.BankConfig;
 import org.example.service.AccountService;
+import org.example.service.ReportService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -24,6 +25,22 @@ public class App
         AccountService accountService = (AccountService) context.getBean(AccountService.class);
         accountService.processDeposit("Alice", 5000.0);
 
+        System.out.println("\nSpring created everything. You did nothing \n");
+        ReportService reportService = context.getBean(ReportService.class);
+        reportService.generateReport();
+
+        // PROOF: Lets list all the beans created by itself
+        System.out.println("\n********* BEANS SPRING CREATED ***********\n");
+        for(String name : context.getBeanDefinitionNames()){
+            if (name.contains("securebank")){
+                System.out.println(" Bean: " + name);
+            }
+        }
+
+        // SINGLETON PROOF: same oject everytime
+        ReportService reportService1 = context.getBean(ReportService.class);
+        ReportService reportService2 = context.getBean(ReportService.class);
+        System.out.println("\n Same ReportService instance ? "+(reportService1 == reportService2));
        ( (AnnotationConfigApplicationContext) (context)).close();
 
 
--- day16/securebank/src/main/java/com/securebank/config/BankConfig.java
+++ day16/securebank/src/main/java/com/securebank/config/BankConfig.java
@@ -1,7 +1,9 @@
 package org.example.config;
 
+import org.example.db.DBConnection;
 import org.example.service.AccountService;
 import org.example.service.NotificationService;
+import org.example.service.ReportService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,5 +20,17 @@ public class BankConfig {
     @Bean
     public AccountService accountService(){
         return new AccountService(notificationService());
+    }
+
+    //Spring Controls when this object is born, with waht value
+    @Bean
+    public DBConnection dbConnection(){
+        return new DBConnection("db.securebank.com",5432);
+    }
+
+    //Spring controls when this is created, injecting databaseConnection
+    @Bean
+    public ReportService reportService(){
+        return new ReportService(dbConnection());
     }
 }
